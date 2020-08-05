04aad7b5ca87a343e803baa9ff2320b64620611e
Vernonicuhhh <60453038+Vernonicuhhh@users.noreply.github.com>
2020-08-04 19:29:45 -0700

    poop

--- Testing/Multithreaded/CO version/src/main/java/frc/subsystem/AdressableLights.java
+++ Testing/Multithreaded/CO version/src/main/java/frc/subsystem/AdressableLights.java
@@ -1,30 +1,23 @@
-package frc.robot;
+package frc.subsystem;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import frc.robot.Constants.MiscConstants;
 
-public class AdressableLights
-    {
-            public AddressableLED m_led= new AddressableLED(6); //placeholder
-            public AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(150);
+public class AdressableLights {
+            public AddressableLED m_led;
+            public AddressableLEDBuffer m_ledBuffer;
         
-    public AdressableLights()
-    {
-    
+    public AdressableLights() {
+        m_led= new AddressableLED(MiscConstants.LED);
+        m_ledBuffer= new AddressableLEDBuffer(150);
         m_led.setLength(m_ledBuffer.getLength());
-            
-            // Set the data
-            m_led.setData(m_ledBuffer);
-            m_led.start();
+        m_led.setData(m_ledBuffer);
+        m_led.start();
     }
 
-   /* public void init()
-    {
-          
-    }
-    */
-    public void setLED()
-    {
+  
+    public void setLED() {
         int j = 1;
         for (var i = 0; i < m_ledBuffer.getLength(); i+=2) {
             

2255d9983ef5ba6286fd5df00f9a71cf623e2055
Vernonicuhhh <60453038+Vernonicuhhh@users.noreply.github.com>
2020-08-04 19:28:23 -0700

    poop

--- Testing/Multithreaded/CO version/src/main/java/frc/subsystem/LED.java
+++ Testing/Multithreaded/CO version/src/main/java/frc/subsystem/LED.java
@@ -1,57 +1,22 @@
-package frc.robot;
+package frc.subsystem;
 
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-class LED
-{
+import frc.robot.Constants.MiscConstants;
+class LED{
    
-    public ArrayList <Double> colorCombos = new ArrayList<>();
-    Spark ledLight= new Spark(45); //placeholder
-    private double colorRamp = 0;
-    boolean ascending = false;
+    public ArrayList <Double> colorCombos;
+    Spark ledLight;
+    private double colorRamp;
+    boolean ascending;
     
 
-    public LED()
-    {
-        
-    }
-
-
-    
-    public void putRandomLED()
-    {
-      double index = Math.random()*.9+-1.0;
-      ledLight.set(index);
-      SmartDashboard.putString("lights", "successful");
-    }
-  
-    public void rampColors() {
-      ascending = colorRamp >= 1 ? false : true;
-      colorRamp += ascending ? 0.0004 : -0.0004;
-      ledLight.set(colorRamp);
-    }
-
-    public void putTeamColors()
-    {
-        int index=colorCombos.size()-1;
-        double value = Math.floor(Math.random()*index+0);
-        ledLight.set(colorCombos.get((int)value));
-        SmartDashboard.putNumber("index", colorCombos.get((int)value));
-    }
-    public void set(double num)
-    {
-      ledLight.set(num);
-    }
-
-    public void blowTheLoad(){ //( ͡° ͜ʖ ͡°)
-      ledLight.set(.77);
-    } 
-
-
-  public void init()
-  {
+    public LED() {
+      ledLight= new Spark(MiscConstants.LED); 
+      colorCombos = new ArrayList<>();
+      ascending = false;
       colorRamp = 0;
       colorCombos.add(-.21);
       colorCombos.add(-.81);
@@ -67,6 +32,37 @@ class LED
       colorCombos.add(.55);
       colorCombos.add(.93);
       colorCombos.add(.63);
-  }
+
+    }
+
+
+    
+    public void putRandomLED(){
+      double index = Math.random()*.9+-1.0;
+      ledLight.set(index);
+      SmartDashboard.putString("lights", "successful");
+    }
+  
+    public void rampColors() {
+      ascending = colorRamp >= 1 ? false : true;
+      colorRamp += ascending ? 0.0004 : -0.0004;
+      ledLight.set(colorRamp);
+    }
+
+    public void putTeamColors(){
+        int index=colorCombos.size()-1;
+        double value = Math.floor(Math.random()*index+0);
+        ledLight.set(colorCombos.get((int)value));
+        SmartDashboard.putNumber("index", colorCombos.get((int)value));
+    }
+    public void set(double num){
+      ledLight.set(num);
+    }
+
+    public void blowTheLoad(){ //( ͡° ͜ʖ ͡°)
+      ledLight.set(.77);
+    } 
+
+
 
 }
